0b5c8642503a176757036b9f1836421838477161
Jean-Frederic Clere <jfclere@gmail.com>
2013-12-13 16:39:43 +0100

    Arrange output.

--- scripts/computemean.sh
+++ scripts/computemean.sh
@@ -1,20 +1,5 @@
 for t in `ls 1500-*-1000000-*.com | sed 's/^1500-//g' | sed 's/-.*$//g' | sort | uniq`
 do
-  echo "Processing 1500-${t}-1000000-*.com"
-  # beginning
-  X=`cat 1500-${t}-1000000-*.com| sort -n -k 2 | tail -1 | awk '{ print $2 }'`
-  # end
-  Y=`cat 1500-${t}-1000000-*.com| sort -n -k 2 | head -1 | awk '{ print $2 }'`
-  # duration in nanoseconds
-  Z=`expr $X - $Y`
-  # duration in seconds
-  T=`perl -le "print $Z/1000000000"`
-  # total number of pkts
-  PKTS=`wc -l 1500-${t}-1000000-*.com | grep total | awk '{ print $1 }'`
-  # load in pkts/s
-  FREQ=`perl -le "print $PKTS/$T"`
-
+  #echo "Processing 1500-${t}-1000000-*.com"
   cat 1500-${t}-1000000-*.com | ./primitif_mean.pl
-  echo "request: ${FREQ}"
-  # > 1500-$t-1000000.dat
 done
--- scripts/primitif_mean.pl
+++ scripts/primitif_mean.pl
@@ -6,6 +6,11 @@ use POSIX;
 $T = 0;
 $max = 0;
 $min = 3000;
+$fist = 0;
+$last = 0;
+
+# all delay values
+@numbers;
 
 while(<STDIN>)
 {
@@ -15,14 +20,38 @@ while(<STDIN>)
 	$B = $A[3] - $A[1];
 	# delay in milliseconds
 	$C = $B / 1000000;
+        if ($first == 0) {
+          $first = $A[1];
+        }
+        $first = $A[1] if $first > $A[1];
+        $last = $A[1] if $last < $A[1];
 	# round the delay to milliseconds
 	$D = ceil($C);
         $T = $T + $D;
         $max = $D if $max < $D;
         $min = $D if $min > $D;
+        # store it for standard-deviation
+        $numbers[$i] = $D;
 	# increase the total number of packets
 	$i++;
 }
 
 $mean = $T / $i;
-print "item: $i max: $max min: $min total: $T mean: $mean ";
+
+$sum = 0;
+# calculate standard deviation:
+for($j = 1; $j < $i; $j++) {
+  $S = $numbers[$j] - $mean;
+  $sum = $sum + ($S * $S);
+}
+$std = sqrt($sum / $i);
+
+# duration in nanoseconds
+$Z = $last - $first;
+# duration in seconds
+$Time = $Z / 1000000000;
+$FREQ = $i / $Time;
+
+#print "item: $i max: $max min: $min total: $T mean: $mean std: $std request: $FREQ \n";
+
+print "$FREQ $mean $std $std\n";
